391a49850eafa381ab436d71ef77a914ab1cc9bd
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-14 14:24:58 +0530

    Initial project architecture

--- README.md
+++
@@ -1,1 +0,0 @@
-# appointly-web
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p className="text-2xl text-center mt-20">Welcome to Appointly</p>
+    </>
+  );
+}
+
+export default App;
